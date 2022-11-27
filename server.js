91f70dea2f9decccdcd52fd0cfdb54a033395c95
mihai.sturza <mihai.sturza@lateral-inc.com>
2022-11-27 15:28:33 +0200

    morgan third party lib

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
+const bootcamps = require('./routes/bootcamps');
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
